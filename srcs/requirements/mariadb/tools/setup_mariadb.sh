c2519c5c57b653a6370e62a35269a05b15b3ef45
Giuseppe Vigilante <gvigilan@e3r5p5.42roma.it>
2025-05-30 15:19:56 +0200

    Inceeption - Fix MDB Dockerfile

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -4,15 +4,6 @@ echo Starting MariaDB Service...
 
 service mariadb start
 
-# Wait until MariaDB is ready
-until mariadb -uroot -e "SELECT 1;" >/dev/null 2>&1; do
-  echo "Waiting for MariaDB to start..."
-  sleep 1
-done
-
-
-service mariadb start
-
 	mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 	mariadb -uroot -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
 	mariadb -uroot -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
