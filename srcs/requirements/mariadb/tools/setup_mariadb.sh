3479eac8c140807aba27a24f18b891c61693e8dd
Giuseppe Vigilante <gvigilan@e3r5p5.42roma.it>
2025-05-30 17:43:10 +0200

    Inception - MARIADBBBBBB

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+set -e
 
-service mariadb start
+mysqld_safe &
 
-	mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-	mariadb -uroot -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
-	mariadb -uroot -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-	mariadb -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-	mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+until mariadb -uroot -e "SELECT 1" &>/dev/null; do
+    echo "Waiting for MariaDB to be ready..."
+    sleep 1
+done
+
+mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mariadb -uroot -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -uroot -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 	
 mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
 
-exec mysqld_safe
+wait
--- srcs/requirements/nginx/tools/nginx_start.sh
+++ srcs/requirements/nginx/tools/nginx_start.sh
@@ -4,7 +4,7 @@ echo "Nginx: setting up ssl ...";
 openssl req -x509 -nodes -days 365 \
         -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key \
         -out /etc/ssl/certs/nginx.crt \
-        -subj "/C=IT/ST=Rome/L=Rome/O=wordpress/CN=gvigilan.42.fr";
+        -subj "/C=IT/ST=Lazio/L=Rome/O=wordpress/CN=gvigilan.42.fr";
 echo "Nginx: ssl is set up!";
 fi
 exec "$@"
