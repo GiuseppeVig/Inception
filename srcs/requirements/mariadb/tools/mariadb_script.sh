303d885f6616bb7e8ddfe2fffb1b63e41ec104b6
Giuseppe Vigilante <gvigilan@e3r5p5.42roma.it>
2025-05-30 23:17:17 +0200

    Inception - forcing TCP

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -7,12 +7,12 @@ fi
 
 service mariadb start
 
-	mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-	mariadb -uroot -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
-	mariadb -uroot -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-	mariadb -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-	mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+	mariadb --protocol=TCP -h127.0.0.1 -uroot -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+	mariadb --protocol=TCP -h127.0.0.1 -uroot -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	mariadb --protocol=TCP -h127.0.0.1 -uroot -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	mariadb --protocol=TCP -h127.0.0.1 -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+	mariadb --protocol=TCP -h127.0.0.1 -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 	
-mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
+mysqladmin --protocol=TCP -h127.0.0.1 -uroot -p$MYSQL_ROOT_PASSWORD shutdown
 
 exec mysqld --defaults-file=/etc/mysql/mariadb.conf.d/mdb.cnf
