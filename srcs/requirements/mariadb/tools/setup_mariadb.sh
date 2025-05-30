6eea39a5082cdf13608158cd75d9cea642af1078
Giuseppe Vigilante <gvigilan@e3r5p5.42roma.it>
2025-05-30 18:53:58 +0200

    Inception - New changes to MDB

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -x
 
 # mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 # mariadb -uroot -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
@@ -11,6 +12,24 @@ set -e
 
 # exec mysqld_safe --datadir=/var/lib/mysql
 
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
+# Start mysqld in background
+mysqld_safe --datadir=/var/lib/mysql &
+
+# Wait for it to be ready
+MAX_TRIES=30
+TRIES=0
+until mariadb -uroot -e "SELECT 1" &>/dev/null; do
+    if [ "$TRIES" -ge "$MAX_TRIES" ]; then
+        echo "ERROR: MariaDB did not become ready in time"
+        exit 1
+    fi
+    echo "Waiting for MariaDB... Attempt $((TRIES+1))/$MAX_TRIES"
+    TRIES=$((TRIES+1))
+    sleep 1
+done
 
 mariadb -uroot <<-EOSQL
     CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
@@ -19,3 +38,6 @@ mariadb -uroot <<-EOSQL
     ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
     FLUSH PRIVILEGES;
 EOSQL
+
+mysqladmin -uroot -p"${MYSQL_ROOT_PASSWORD}" shutdown
+exec mysqld_safe --datadir=/var/lib/mysql
