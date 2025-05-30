22f319ba2b5f7c1f1f5496030ec1b88935b30d5a
Giuseppe Vigilante <gvigilan@e3r5p5.42roma.it>
2025-05-30 14:40:51 +0200

    Inceeption - Fix env and setups

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -1,41 +1,13 @@
 #!/bin/bash
-set -e
 
-# Initialize database if empty
-if [ ! -d /var/lib/mysql/mysql ]; then
-    echo "Initializing database..."
-    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-fi
+service mariadb start
 
-# Start MariaDB in background
-echo "Starting MariaDB in background..."
-mysqld_safe --datadir=/var/lib/mysql &
-pid="$!"
+	mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+	mariadb -uroot -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	mariadb -uroot -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	mariadb -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_USER';"
+	mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+	
+mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
 
-# Wait for socket to exist
-echo "Waiting for MariaDB to be ready..."
-until [ -S /run/mysqld/mysqld.sock ]; do
-    sleep 1
-done
-
-echo "MariaDB socket found!"
-
-# Run setup if database doesn't exist
-if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
-    echo "Setting up database..."
-    mysql -u root <<-EOSQL
-        CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-        GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-        ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-        FLUSH PRIVILEGES;
-EOSQL
-fi
-
-# Shutdown the temporary server
-echo "Shutting down MariaDB setup instance..."
-mysqladmin -u root --password="${MYSQL_ROOT_PASSWORD}" shutdown
-
-# Start final MariaDB server
-echo "Starting MariaDB foreground..."
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -7,10 +7,20 @@ if [ -f ./wp-config.php ]
 then
 	echo "Wordpress already exists"
 else
-	wp core download --allow-root
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
-	wp core install --url=$DONAIN_NAME --title="$WORDPRESS_TITLE" --admin_user=$WORDPRESS_ADMIM --admin_password=$WORDPRESS_ADMIM_PASS  --admin_email=$WORDPRESS_ADMIM_EMAIL --skip-email --allow-root
-	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASS --allow-root
+	wp config create --dbhost=$MYSQL_HOSTNAME \
+					 --dbname=$MYSQL_DATABASE \
+					 --dbuser=$MYSQL_USER \
+					 --dbpass=$MYSQL_PASSWORD \
+					 --path="/var/www/inception" --allow-root
+
+	wp core install --url=$DONAIN_NAME \
+					--title="$WORDPRESS_TITLE" \
+					--admin_user=$WORDPRESS_ADMIM \
+					--admin_password=$WORDPRESS_ADMIM_PASS  \
+					--admin_email=$WORDPRESS_ADMIM_EMAIL --allow-root
+	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --user_pass=$WORDPRESS_USER_PASS \
+					--role=author  --allow-root
+	
 	wp theme install twentysixteen --activate --allow-root
 fi
 
