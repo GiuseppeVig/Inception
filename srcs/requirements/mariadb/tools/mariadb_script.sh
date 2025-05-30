72a147b9bf3e23b879191057d88903a4a677811f
Giuseppe Vigilante <gvigilan@e3r5p5.42roma.it>
2025-05-30 23:45:59 +0200

    Inception new approch

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+mysql_install_db --user=mysql --datadir=/var/lib/mysql &
+
+sleep 20
+
+echo "Starting MariaDB with custom config only..."
+
 mysqld --defaults-file=/etc/mysql/mariadb.conf.d/mdb.cnf &
+
+sleep 20
 
 service mariadb start
 
-	mariadb --protocol=TCP -h127.0.0.1 -uroot -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-	mariadb --protocol=TCP -h127.0.0.1 -uroot -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
-	mariadb --protocol=TCP -h127.0.0.1 -uroot -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-	mariadb --protocol=TCP -h127.0.0.1 -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-	mariadb --protocol=TCP -h127.0.0.1 -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+	mariadb --protocol=TCP -h127.0.0.1 -P3306 -uroot -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+	mariadb --protocol=TCP -h127.0.0.1 -P3306 -uroot -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	mariadb --protocol=TCP -h127.0.0.1 -P3306 -uroot -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	mariadb --protocol=TCP -h127.0.0.1 -P3306 -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+	mariadb --protocol=TCP -h127.0.0.1 -P3306 -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 	
-mysqladmin --protocol=TCP -h127.0.0.1 -uroot -p$MYSQL_ROOT_PASSWORD shutdown
+mysqladmin --protocol=TCP -h127.0.0.1 -P3306 -uroot -p$MYSQL_ROOT_PASSWORD shutdown
 
 exec mysqld --defaults-file=/etc/mysql/mariadb.conf.d/mdb.cnf
+
+wait
